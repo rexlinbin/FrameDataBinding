16c3f7ccec4843c9cd289f488edaf245f633fb68
linbin <rexlinbin@126.com>
2020-12-07 14:50:24 +0800

    添加拦截器

--- frame_databinding/src/main/java/com/lengyue/frame_databinding/network/RetrofitManager.java
+++ frame_databinding/src/main/java/com/lengyue/frame_databinding/network/RetrofitManager.java
@@ -5,6 +5,7 @@ import com.lengyue.frame_databinding.network.Interceptor.HttpLogInterceptor;
 
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -44,6 +45,26 @@ public class RetrofitManager<T> {
         okHttpClient = new OkHttpClient.Builder()
                 //设置日志打印
                 .addInterceptor(new HttpLogInterceptor())
+                //失败重连
+                .retryOnConnectionFailure(true)
+                //网络请求超时时间单位为秒
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(DEFAULT_SERVER)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+        retrofitApiService = (T) retrofit.create(classz);
+    }
+
+    public void initRetrofit(String url, Class classz, Interceptor interceptor) {
+        DEFAULT_SERVER = url;
+        okHttpClient = new OkHttpClient.Builder()
+                //设置日志打印
+                .addInterceptor(interceptor)
                 //失败重连
                 .retryOnConnectionFailure(true)
                 //网络请求超时时间单位为秒
